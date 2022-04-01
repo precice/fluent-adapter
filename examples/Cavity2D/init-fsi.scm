b012e9df27dd4bea2e3c84bad3d9bfb8183daab9
Mike Tree <mtree@corvidtec.com>
2022-04-01 16:16:53 -0400

    time step issues all sorted out except there is no support for adaptive time stepping; getting an error where preCICE complains about being passed NaN or Inf forces

--- examples/Cavity2D/init-fsi.scm
+++ examples/Cavity2D/init-fsi.scm
@@ -1,22 +1,10 @@
-; create scheme variables if they don't already exist
-(if (not (rp-var-object 'udf/convergence))
-   (rp-var-define 'udf/convergence 1 'int #f)
-)
-(if (not (rp-var-object 'udf/ongoing))
-   (rp-var-define 'udf/ongoing 1 'int #f)
-)
-(if (not (rp-var-object 'udf/iterate))
-   (rp-var-define 'udf/iterate 0 'int #f)
-)
-(if (not (rp-var-object 'udf/checkpoint))
-   (rp-var-define 'udf/checkpoint 0 'int #f)
-)
-(if (not (rp-var-object 'udf/config-location))
-   (rp-var-define 'udf/config-location "./precice-config.xml" 'string #f)
-)
-(if (not (rp-var-object 'solve/dt))
-   (rp-var-define 'solve/dt "0.5" 'string #f)
-)
+; create scheme variables
+(rp-var-define 'udf/convergence 1 'int #f)
+(rp-var-define 'udf/ongoing 1 'int #f)
+(rp-var-define 'udf/iterate 0 'int #f)
+(rp-var-define 'udf/checkpoint 0 'int #f)
+(rp-var-define 'udf/config-location "./precice-config.xml" 'string #f)
+(rp-var-define 'solve/dt 0.5 'real #f)
 
 ; assign values to variables created
 (rpsetvar 'udf/convergence 1)
@@ -24,11 +12,15 @@
 (rpsetvar 'udf/iterate 0)
 (rpsetvar 'udf/checkpoint 0)
 
+; make sure case is using fixed time stepping method
+(rpsetvar 'time/adaptive/dt-method 0)
+
 ;;; USER CAN EDIT THESE ;;;
 (rpsetvar 'udf/config-location "./precice-config.xml")
-(rpsetvar 'solve/dt "0.5")
+(rpsetvar 'solve/dt 1.0)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; initialize flow field
+(ti-menu-load-string (string-append "solve/set/time-step " (number->string(%rpgetvar 'solve/dt))))
 (rpsetvar 'dynamesh/update-in-timestep/residual-criterion -1.0)
 (ti-menu-load-string "solve/initialize/initialize-flow")
